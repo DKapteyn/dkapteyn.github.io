d23a30f7b326c5f995e92cc55a3e8d1f58a39b89
David Kapteyn <dave.kapteyn@gmail.com>
2022-07-04 14:47:37 -0600

    dynamic styling added to Skills container

--- src/components/Skills/SkillsContainer.tsx
+++ src/components/Skills/SkillsContainer.tsx
@@ -2,25 +2,35 @@ import Skill from "./Skill";
 
 export default function SkillsContainer() {
   return (
-    <div className="grid place-items-center mt-8">
+    <div className="flex flex-col w-full items-center  mt-8">
       <div>
         <h2 className="text-xl">Technical Skills</h2>
       </div>
-      <div className="flex gap-8">
-        <Skill skill="React" skillImg=".\images\logos\react-svgrepo-com.svg" />
-        <Skill
-          skill="Javascript"
-          skillImg=".\images\logos\javascript-logo-svgrepo-com.svg"
-        />
-        <Skill
-          skill="Typescript"
-          skillImg=".\images\logos\typescript-logo-svgrepo-com.svg"
-        />
-        <Skill
-          skill="Tailwind CSS"
-          skillImg=".\images\logos\tailwindcss-icon-svgrepo-com.svg"
-        />
-        <Skill skill="Sass" skillImg=".\images\logos\sass-svgrepo-com.svg" />
+
+      <div className="flex flex-col md:flex-row md:gap-8">
+        <div className="skillIcon">
+          <Skill
+            skill="React"
+            skillImg=".\images\logos\react-svgrepo-com.svg"
+          />
+        </div>
+        <div className="skillIcon">
+          <Skill
+            skill="Javascript"
+            skillImg=".\images\logos\javascript-logo-svgrepo-com.svg"
+          />
+          <Skill
+            skill="Typescript"
+            skillImg=".\images\logos\typescript-logo-svgrepo-com.svg"
+          />
+        </div>
+        <div className="skillIcon">
+          <Skill
+            skill="Tailwind CSS"
+            skillImg=".\images\logos\tailwindcss-icon-svgrepo-com.svg"
+          />
+          <Skill skill="Sass" skillImg=".\images\logos\sass-svgrepo-com.svg" />
+        </div>
       </div>
     </div>
   );
